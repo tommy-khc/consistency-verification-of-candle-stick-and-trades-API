5d3831ca61ac2737359dae17cc35f00b74d88dd6
tomycc <tom123abc@gmail.com>
2022-04-06 01:42:33 -0700

    add L for long

--- src/main/java/Entity/TimeFrame.java
+++ src/main/java/Entity/TimeFrame.java
@@ -5,18 +5,18 @@ import org.apache.logging.log4j.Logger;
 
 public enum TimeFrame {
 
-    ONE_MINUTE("1m", 60000),
-    FIVE_MINUTE("5m", 300000),
-    FIFTEEN_MINUTE("15m", 900000),
-    THIRTY_MINUTE("30m", 1800000),
-    ONE_HOUR("1h", 3600000),
-    FOUR_HOUR("4h", 14400000),
-    SIX_HOUR("6h", 21600000),
-    TWELEVE_HOUR("12h", 43200000),
-    ONE_DAY("1D", 86400000),
-    ONE_WEEK("7D", 604800000),
-    TWO_WEEKS("14D", 1210000000),
-    ONE_MONTH("1M", -1); //2,628,000,000 is not accurate and it is too large for long
+    ONE_MINUTE("1m", 60000L),
+    FIVE_MINUTE("5m", 300000L),
+    FIFTEEN_MINUTE("15m", 900000L),
+    THIRTY_MINUTE("30m", 1800000L),
+    ONE_HOUR("1h", 3600000L),
+    FOUR_HOUR("4h", 14400000L),
+    SIX_HOUR("6h", 21600000L),
+    TWELEVE_HOUR("12h", 43200000L),
+    ONE_DAY("1D", 86400000L),
+    ONE_WEEK("7D", 604800000L),
+    TWO_WEEKS("14D", 1210000000L),
+    ONE_MONTH("1M", 2629800000L); //2,628,000,000 is not accurate and it is too large for long
 
     private final String shortName;
 
